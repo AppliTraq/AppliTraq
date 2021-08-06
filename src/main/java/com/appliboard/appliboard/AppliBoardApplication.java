592aacbf61f19c1103329a6ad8e9da7579092657
Hannah Paterka <paterkahannah@gmail.com>
2021-08-06 12:01:05 -0500

    first commit

---
+++ src/main/java/com/appliboard/appliboard/AppliBoardApplication.java
@@ -0,0 +1,13 @@
+package com.appliboard.appliboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliBoardApplication.class, args);
+    }
+
+}
