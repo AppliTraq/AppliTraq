dac9575ecb78783acaa3f911e6c0fd7183d0fe44
Karen Valadez <valadez.karen04@gmail.com>
2021-08-06 16:36:56 -0500

    created and completed model for timeline table

---
+++ src/main/java/com/appliboard/appliboard/models/TimelineTable.java
@@ -0,0 +1,71 @@
+package com.appliboard.appliboard.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "timeline")
+public class TimelineTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long timeline_id;
+
+    @OneToMany
+    @JoinColumn(name = "job_id")
+    private JobApplication jobApplications;
+
+    @Column(nullable = false, columnDefinition = "DATE")
+    private Date date;
+
+    @Column(nullable = false)
+    private int kanban_status;
+
+    // constructors
+    public TimelineTable() {
+    }
+
+    public TimelineTable(long timeline_id, JobApplication jobApplications, Date date, int kanban_status) {
+        this.timeline_id = timeline_id;
+        this.jobApplication = jobApplications;
+        this.date = date;
+        this.kanban_status = kanban_status;
+    }
+
+    // setters and getters
+    public long getTimeline_id() {
+        return timeline_id;
+    }
+
+    public void setTimeline_id(long timeline_id) {
+        this.timeline_id = timeline_id;
+    }
+
+    public JobApplication getJobApplications() {
+        return jobApplications;
+    }
+
+    public void setJobApplications(JobApplication jobApplications) {
+        this.jobApplications = jobApplications;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getKanban_status() {
+        return kanban_status;
+    }
+
+    public void setKanban_status(int kanban_status) {
+        this.kanban_status = kanban_status;
+    }
+}
