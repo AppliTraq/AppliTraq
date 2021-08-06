57d8ca448e2fc8babe9b5b972afd362d0c4d9f47
Lex Zavala <lex.zavala7@gmail.com>
2021-08-06 15:35:37 -0500

    created user model class

---
+++ src/main/java/com/appliboard/appliboard/models/User.java
@@ -0,0 +1,99 @@
+package com.appliboard.appliboard.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column (nullable = false)
+    private String username;
+    @Column (nullable = false)
+    private String email;
+    @Column (nullable = false)
+    private String password;
+    @Column
+    private String gender;
+    @Column
+    private int age;
+    @Column
+    private String location;
+
+    @OneToMany (cascade = CascadeType.ALL, mappedBy = "user")
+    private List<JobApplication> jobApp;
+
+
+
+    public User() {
+    }
+
+    public User(long id, String username, String email, String password, String gender, int age, String location) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
