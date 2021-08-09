4b84ad4187db6174e7357de0f17a9574da8c3b71
Karen Valadez <valadez.karen04@gmail.com>
2021-08-09 16:54:32 -0500

    added timeline html and timeline controller

--- src/main/java/com/appliboard/appliboard/controllers/TimelineController.java
+++ src/main/java/com/appliboard/appliboard/controllers/TimelineController.java
@@ -18,7 +18,7 @@ public class TimelineController {
     @GetMapping("/timeline")
     public String timelineShow(Model model) {
         model.addAttribute("job", jobApplicationDao.findAll());
-        return "users/timeline"; // might change?
+        return "timeline/timeline"; // might change?
     }
 
 }
