0f3dae103a2e3887643ae7ed0efada0f92a28640
Lex Zavala <lex.zavala7@gmail.com>
2021-08-09 11:29:34 -0500

    created user details loader class and a model of user with roles to resemble the structure on our springblog application

--- src/main/java/com/appliboard/appliboard/services/SecurityConfiguration.java
+++ src/main/java/com/appliboard/appliboard/services/SecurityConfiguration.java
@@ -1,4 +1,31 @@
 package com.appliboard.appliboard.services;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
 }
