4a45545f53013607141a37a6f15b03a3adc711c9
Karen Valadez <valadez.karen04@gmail.com>
2021-08-09 10:06:57 -0500

    extended jpa repository in timeline

--- src/main/java/com/appliboard/appliboard/repositories/TimelineRepository.java
+++ src/main/java/com/appliboard/appliboard/repositories/TimelineRepository.java
@@ -1,4 +1,10 @@
 package com.appliboard.appliboard.repositories;
 
-public interface TimelineRepository {
+import com.appliboard.appliboard.models.Timeline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimelineRepository extends JpaRepository<Timeline, Long>{
+    // this repository interface is being connected to the Timeline class ; manages data between Java objects and a relational database.
+
+    Timeline findbyId(long id);
 }
