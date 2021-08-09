09d3b6b1ddc2febcc6d9a4d6f1d247a9a0548fd8
Karen Valadez <valadez.karen04@gmail.com>
2021-08-09 16:37:16 -0500

    timeline controller get mapping

--- src/main/java/com/appliboard/appliboard/controllers/TimelineController.java
+++ src/main/java/com/appliboard/appliboard/controllers/TimelineController.java
@@ -1,4 +1,24 @@
 package com.appliboard.appliboard.controllers;
 
+import com.appliboard.appliboard.repositories.JobApplicationRepository;
+import com.appliboard.appliboard.repositories.TimelineRepository;
+import com.appliboard.appliboard.repositories.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TimelineController {
+    private final TimelineRepository timelineDao;
+    private final JobApplicationRepository jobApplicationDao;
+
+    public TimelineController(TimelineRepository timelineDao,  JobApplicationRepository jobApplicationDao) {
+        this.timelineDao = timelineDao;
+        this.jobApplicationDao = jobApplicationDao;
+    }
+
+    @GetMapping("/timeline")
+    public String timelineShow(Model model) {
+        model.addAttribute("job", jobApplicationDao.findAll());
+        return "users/timeline"; // might change?
+    }
+
 }
