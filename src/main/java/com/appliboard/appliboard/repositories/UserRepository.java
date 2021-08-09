b04a287e4df1d716cfde44c8b1d5a5ed0e3c879c
Lex Zavala <lex.zavala7@gmail.com>
2021-08-09 10:07:11 -0500

    completed repository for user

--- src/main/java/com/appliboard/appliboard/repositories/UserRepository.java
+++ src/main/java/com/appliboard/appliboard/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.appliboard.appliboard.repositories;
 
-public interface UserRepository {
+import com.appliboard.appliboard.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+    User findByUsername(String username);
 }
