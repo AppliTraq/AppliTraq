c10748ccaf5fddfd6f78cf04b51aa68402fb46bc
Juan Perez <jperez4432@gmail.com>
2021-08-11 12:54:42 -0500

    notes added for note controller on the final steps needed

--- src/main/java/com/appliboard/appliboard/controllers/NoteController.java
+++ src/main/java/com/appliboard/appliboard/controllers/NoteController.java
@@ -1,5 +1,6 @@
 package com.appliboard.appliboard.controllers;
 
+import com.appliboard.appliboard.models.JobApplication;
 import com.appliboard.appliboard.models.Note;
 import com.appliboard.appliboard.models.User;
 import com.appliboard.appliboard.repositories.NoteRepository;
@@ -8,6 +9,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.PreparedStatement;
 
 @Controller
 public class NoteController {
@@ -21,18 +24,30 @@ public class NoteController {
 
     @GetMapping("/notes/create")
     public String createNoteForm(Model model){
-
         model.addAttribute("note", new Note());
 
         return "/notes/create";
     }
 
+//    Need to either update the note.java file to auto update with the current time bc of the datetime column or insert it below. try the first ideally. then the job_id column in the
+//            table is the jobapplication you see below, need to link that together with the job_app so it inserts into the note controller properly and updates
+
     @PostMapping("/notes/create")
     public String createNote(@ModelAttribute Note note){
+
+        JobApplication jobApplication = (JobApplication) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        //figure out this to implement the timestamp or just do it automatically in some kinda auto increment way in MySQL
+//        java.sql.Timestamp date = new java.sql.Timestamp(new java.util.Date().getTime());
+//        PreparedStatement.setTimestamp(1, date);
+
+//        note.setJobApplication(jobApplication.getId(jobApplication));
+            note.setJobApplication(jobApplication);
 //        note.getJobApplication()
 //        note.setNote_id(user.getId());
-//        note.setNote_id(noteDao.getById(1));
+//        maybe try this way below as a test?
+//        note.setNote_id(1);
         noteDao.save(note);
         return "/notes/index";
     }
@@ -43,10 +58,11 @@ public class NoteController {
         return "/notes/index";
     }
 
-    @PostMapping("/notes/{id}/delete")
+    @PostMapping("/notes/delete/{id}")
     public String deleteNote(@PathVariable long id){
-        noteDao.delete(noteDao.findById(id));
-        return "redirect:/notes/ads";
+//        noteDao.delete(noteDao.findById(id));
+        noteDao.deleteById(id);
+        return "redirect:/notes/index";
     }
 
 }
