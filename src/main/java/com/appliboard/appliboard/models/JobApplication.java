e566b3c04fa1aa7e6c19d565f35ba38bd05a40da
Lex Zavala <lex.zavala7@gmail.com>
2021-08-12 16:29:52 -0500

    added a comment on the job application model

--- src/main/java/com/appliboard/appliboard/models/JobApplication.java
+++ src/main/java/com/appliboard/appliboard/models/JobApplication.java
@@ -46,6 +46,7 @@ public class JobApplication {
 
     public JobApplication () {}
 
+// THIS CONSTRUCTOR IS MISSING A TIMELINE OBJECT BEING PASSED ON SO WE CAN CREATE A GETTER AND SETTER OF THE TIMELINE
     public JobApplication(long id, User user, String title, String description, String company, double salary, String location, String logo) {
         this.id = id;
         this.user = user;
