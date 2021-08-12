cf4d193befefb9cd35dc8a2502205a536b375cb1
Lex Zavala <lex.zavala7@gmail.com>
2021-08-12 00:20:21 -0500

    created get mapping on user controller for deleting the page and added a delete html

--- src/main/java/com/appliboard/appliboard/controllers/UserController.java
+++ src/main/java/com/appliboard/appliboard/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,6 +34,12 @@ public class UserController {
         user.setPassword(hash);
         users.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String takeToDelete (@PathVariable long id, Model model) {
+
+        return ""
     }
 
 }
