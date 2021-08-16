a5862a2624e2b5baaadf2b1b185ffc5ea8926dbc
Hannah Paterka <paterkahannah@gmail.com>
2021-08-16 16:55:29 -0500

    logo work

--- src/main/java/com/appliboard/appliboard/services/EmailService.java
+++ src/main/java/com/appliboard/appliboard/services/EmailService.java
@@ -10,26 +10,26 @@ import org.springframework.stereotype.Service;
 
 @Service("mailService")
 public class EmailService {
-    @Autowired
-    public JavaMailSender emailSender;
-
-    @Value("applitraq@example.com") // email that is always sending (ex: no reply emails)
-    private String from;
-
-    public void prepareAndSend(Reminder reminder, String subject, String body) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom(from);
-        msg.setTo(reminder.getDescription()); // changes from 'to' to post so the post can be sent instead
-        msg.setSubject(subject);
-        msg.setText(body);
-
-        try{
-            this.emailSender.send(msg);
-        }
-        catch (MailException ex) {
-            // simply log it and go on...
-            System.err.println(ex.getMessage());
-        }
-    }
+//    @Autowired
+//    public JavaMailSender emailSender;
+//
+//    @Value("applitraq@example.com") // email that is always sending (ex: no reply emails)
+//    private String from;
+//
+//    public void prepareAndSend(Reminder reminder, String subject, String body) {
+//        SimpleMailMessage msg = new SimpleMailMessage();
+//        msg.setFrom(from);
+//        msg.setTo(reminder.getDescription()); // changes from 'to' to post so the post can be sent instead
+//        msg.setSubject(subject);
+//        msg.setText(body);
+//
+//        try{
+//            this.emailSender.send(msg);
+//        }
+//        catch (MailException ex) {
+//            // simply log it and go on...
+//            System.err.println(ex.getMessage());
+//        }
+//    }
 
 }
