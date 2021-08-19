bafa07a525e4f02278e42ce6c7c89483f8b80680
Karen Valadez <valadez.karen04@gmail.com>
2021-08-19 16:12:37 -0500

    EMAIL WORKS c':

--- src/main/java/com/appliboard/appliboard/controllers/ReminderController.java
+++ src/main/java/com/appliboard/appliboard/controllers/ReminderController.java
@@ -5,17 +5,13 @@ import com.appliboard.appliboard.models.User;
 import com.appliboard.appliboard.repositories.JobApplicationRepository;
 import com.appliboard.appliboard.repositories.ReminderRepository;
 import com.appliboard.appliboard.repositories.UserRepository;
-import com.appliboard.appliboard.services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
+import com.appliboard.appliboard.services.EmailService;;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-import java.util.Date;
 
 @Controller
 public class ReminderController {
