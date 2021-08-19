9c60a1b1f37b94d05c393353c3d11a85381fe93c
Karen Valadez <valadez.karen04@gmail.com>
2021-08-19 16:10:16 -0500

    EMAIL WORKS c':

--- src/main/java/com/appliboard/appliboard/services/EmailService.java
+++ src/main/java/com/appliboard/appliboard/services/EmailService.java
@@ -1,72 +1,42 @@
-//package com.appliboard.appliboard.services;
-//
-//import com.appliboard.appliboard.models.JobApplication;
-//import com.appliboard.appliboard.models.Reminder;
-//import com.appliboard.appliboard.models.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.MailException;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//import java.util.Arrays;
-//
-//@Service("mailService")
-//public class EmailService {
-//
-//    @Autowired
-//    public JavaMailSender emailSender;
-//
-//    @Value("${spring.mail.from}") // email that is always sending (ex: no reply emails)
-//    private String from;
-//
-//    public void prepareAndSend(User user, String reminderSelect, JobApplication jobApp) { // job application added
-//        SimpleMailMessage msg = new SimpleMailMessage();
-//        msg.setFrom(from);
-//        System.out.println(msg.getFrom());
-//        msg.setTo(user.getEmail()); // changes from 'to' to post so the post can be sent instead
-//        System.out.println(Arrays.toString(msg.getTo()));
-//        // gives reminder options to the user
-////        switch(reminderSelect) {
-////            case "email":
-//                String subject = "Remember to follow up with " ;
-//        System.out.println(subject);
-//                msg.setSubject(subject);
-//        System.out.println(msg.getSubject());
-//
-//                String body = jobApp.getTitle();
-//        System.out.println(body);
-//                msg.setText(body);
-//        System.out.println(msg.getText());
-//                break;
-//
-//            case "appPortal":
-//                subject = "Remember to check the application portal for " + jobApp.getCompany(); // check status on app, etc.
-//                msg.setSubject(subject);
-//
-//                body = "INFO OF APP" + jobApp.getDescription();
-//                msg.setText(body);
-//                break;
-//
-//            case "research":
-//                subject = "Remember to research on the following company: " + jobApp.getCompany();
-//                msg.setSubject(subject);
-//
-//                body = "RESEARCH INFO" + jobApp.getDescription();
-//                msg.setText(body);
-//                break;
-//        }
-//
-//
-//        try{
-//            emailSender.send(msg);
-//        }
-//        catch (MailException ex) {
-//            // simply log it and go on...
-//            System.err.println(ex.getMessage());
-//        }
-//    }
-//
-//}
+package com.appliboard.appliboard.services;
+import com.appliboard.appliboard.models.JobApplication;
+import com.appliboard.appliboard.models.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.sendgrid.*;
+import java.io.IOException;
+
+@Service("mailService")
+public class EmailService {
+    @Value("${sgkey}")
+    private String sgkey;
+
+    public void sendEmail(User user, String reminderSelect, JobApplication jobApp) throws IOException {
+
+        Email from = new Email("applitraq@gmail.com");
+        System.out.println(from.getEmail());
+        String subject = "Applitraq Reminder";
+        System.out.println(subject);
+        Email to = new Email(user.getEmail());
+        System.out.println(to.getEmail());
+        Content content = new Content("text/plain", jobApp.getTitle() + ", " +jobApp.getDescription());
+        System.out.println(content.getValue());
+
+        Mail mail = new Mail(from, subject, to, content);
+        System.out.println(mail.subject);
+
+        SendGrid sg = new SendGrid(sgkey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+}
