461b4a76b82a115adc7b74ecec963055dffaf541
Lex Zavala <lex.zavala7@gmail.com>
2021-08-18 23:20:09 -0500

    messing with the job create controller

--- src/main/java/com/appliboard/appliboard/controllers/JobApplicationController.java
+++ src/main/java/com/appliboard/appliboard/controllers/JobApplicationController.java
@@ -6,8 +6,8 @@ import com.appliboard.appliboard.models.Timeline;
 import com.appliboard.appliboard.models.User;
 import com.appliboard.appliboard.repositories.JobApplicationRepository;
 import com.appliboard.appliboard.repositories.NoteRepository;
+import com.appliboard.appliboard.repositories.TimelineRepository;
 import com.appliboard.appliboard.repositories.UserRepository;
-import org.apache.catalina.LifecycleState;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -15,7 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 
@@ -24,11 +24,13 @@ public class JobApplicationController {
     private final JobApplicationRepository jobApplicationDao;
     private final UserRepository usersDao;
     private final NoteRepository noteDao;
+    private final TimelineRepository timelineDao;
 
-    public JobApplicationController(JobApplicationRepository jobApplicationDao, UserRepository usersDao, NoteRepository noteDao) {
+    public JobApplicationController(JobApplicationRepository jobApplicationDao, UserRepository usersDao, NoteRepository noteDao, TimelineRepository timelineDao) {
         this.jobApplicationDao = jobApplicationDao;
         this.usersDao = usersDao;
         this.noteDao = noteDao;
+        this.timelineDao = timelineDao;
     }
 
 //    VIEW ALL
@@ -77,8 +79,12 @@ public class JobApplicationController {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         jobApp.setUser(user);
         Timeline timeline = new Timeline(jobApp, Date.from(Instant.now()), 1);
-        jobApp.setTimeline(List.of(timeline));
+        List<Timeline> newListOfTimelineStatus = new ArrayList<Timeline>();
+        newListOfTimelineStatus.add(timeline);
+        jobApp.setTimeline(newListOfTimelineStatus);
+        System.out.println(jobApp.getTimeline());
         jobApplicationDao.save(jobApp);
+//        timelineDao.save(timeline);
         return "redirect:/jobApplications/";
     }
 
@@ -116,19 +122,22 @@ public class JobApplicationController {
     public String updateKanbanStatus(@RequestParam(name = "kanban_status") int kanbanStatus, @RequestParam(name = "jobId") List <Long> jobIds) {
         System.out.println(jobIds);
         int lastIndex = jobIds.size() - 1;
-        System.out.println(lastIndex);
-        JobApplication ja = jobApplicationDao.getById(jobIds.get(lastIndex));
-        List<Timeline> timelineList = ja.getTimeline();
-        System.out.println(ja.getId());
+        JobApplication jobApp = jobApplicationDao.getById(jobIds.get(lastIndex));
+        System.out.println("get timeline "  + jobApp.getTimeline());
+        System.out.println( timelineDao.findTimelinesByJobApplications(jobApp.getId()));
+        List<Timeline> timelineList = jobApp.getTimeline();
+
+        System.out.println(jobApp.getId());
         System.out.println("This is kanban status" + kanbanStatus);
-//        for (Timeline timeline : timelineList) {
-//            System.out.println(ja.getId());
-//            System.out.println(timeline.getJobApplications().getId());
-//            if (ja.getId() == timeline.getJobApplications().getId()){
-//                timeline.setKanban_status(kanbanStatus + 1);
+        System.out.println("This is size of timeline list" + timelineList.size());
+        for (Timeline timeline : timelineList) {
+            System.out.println(timeline.getJobApplications().getId());
+//            if (jobApp.getId() == timeline.getJobApplications().getId()){
+//                timeline.setKanban_status(kanbanStatus);
 //                System.out.println(timeline.getKanban_status());
 //            }
-//        }
+//            timelineDao.save(timeline);
+        }
         System.out.println("This submit works");
         return "redirect:/jobApplications";
     }
--- src/main/java/com/appliboard/appliboard/repositories/TimelineRepository.java
+++ src/main/java/com/appliboard/appliboard/repositories/TimelineRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 public interface TimelineRepository extends JpaRepository<Timeline, Long>{
     // this repository interface is being connected to the Timeline class ; manages data between Java objects and a relational database.
 
+        List<Timeline> findTimelinesByJobApplications (long id);
+
 
 
 
