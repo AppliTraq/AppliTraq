26ec49fde37ff9fc25fdf4c42374f3698491a030
Rose Barcus <angela.rose.barcus@gmail.com>
2021-08-30 17:58:12 -0500

    messing with the seeder again

--- seeder2.sql
+++ seeder2.sql
@@ -2,31 +2,41 @@ INSERT INTO applitraq_db.users (age, email, gender, location, password, username
     VALUES (25, 'user@mail.com', 'Female', 'Los Angeles, CA', '$2a$10$WD2g7r4fh
 .45PPylOp7puuDIimUZej1IJks1vXNrACLI7EPElxAVK', 'user');
 
-INSERT INTO appliboard_db.job_applications (company, description, location, salary, title, user_id)
+INSERT INTO applitraq_db.job_applications (company, description, location, salary, title, user_id)
     VALUES ('Google', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
 been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Las Angeles, CA', 75000, 'Software Developer', 1),
+scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
+versions of Lorem Ipsum.', 'Las Angeles, CA', 75000, 'Software Developer', 2),
     ('Apple', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
 classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
-Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'San Antonio, TX', 70000, 'Software Engineer', 1),
+Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'San Antonio, TX', 70000, 'Software Engineer', 2),
     ('Netflix', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered
 alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you
-are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Austin, TX', 80000, 'Web Developer', 1),
+are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the
+middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Austin, TX', 80000, 'Web Developer', 2),
     ('Amazon', 'It is a long established fact that a reader will be distracted by the readable content of a page when
 looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 'Huntington, CA', 90000, 'Software Developer II', 1),
+as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing
+packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 'Huntington, CA', 90000, 'Software Developer II', 2),
     ('Hulu', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
 industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
-to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Las Angeles, CA', 75000, 'Software Developer', 1),
+to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Las Angeles, CA', 75000, 'Software Developer', 2),
     ('Microsoft', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
 classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
-Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'San Antonio, TX', 65000, 'Software Engineer II', 1),
+Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', 'San Antonio, TX', 65000, 'Software Engineer II', 2),
     ('Home Depot', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered
 alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you
-are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Austin, TX', 80000, 'FullStack Web Developer', 1),
+are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the
+middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Austin, TX', 80000, 'FullStack Web Developer', 2),
     ('Nike', 'It is a long established fact that a reader will be distracted by the readable content of a page when
 looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 'Huntington, CA', 90000, 'Frontend Developer', 1);
+as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing
+packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 'Huntington, CA', 90000, 'Frontend Developer', 2);
 
 INSERT INTO applitraq_db.notes (id, content, date, title, job_id)
 VALUES (1, 'Do take home test 2', ('2021-02-24'), 'Test', 10),
@@ -50,14 +60,14 @@ INSERT INTO applitraq_db.reminders (description, title, job_id)
     ('John Smith', 'Contact', 18);
 
 INSERT INTO applitraq_db.timeline (timeline_id, date, kanban_status, job_applications)
-VALUES (16, '2021-02-24', 4, 11),
-       (17, '2021-03-05', 2, 12),
-       (18, '2021-03-06', 3, 13),
-       (19, '2021-04-20', 3, 14),
-       (20, '2021-05-23', 3, 15),
-       (21, '2021-05-25', 3, 16),
-       (22, '2021-06-06', 3, 17),
-       (23, '2021-07-15', 4, 18);
+VALUES (16, '2021-02-24', 4, 36),
+       (17, '2021-03-05', 2, 37),
+       (18, '2021-03-06', 3, 38),
+       (19, '2021-04-20', 3, 39),
+       (20, '2021-05-23', 3, 40),
+       (21, '2021-05-25', 3, 41),
+       (22, '2021-06-06', 3, 42),
+       (23, '2021-07-15', 4, 43);
 
 
 CREATE TABLE job_applications (
